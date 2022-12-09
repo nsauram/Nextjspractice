744b2cef2f18d7255d811d1669e6ff331e2c32f6
noelia saura martinez <noelia.saura.m@gmail.com>
2022-12-09 14:53:33 +0100

    feat: add css next

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,12 +1,19 @@
 import Navbar from '@components/Navbar/Navbar'
 import React from 'react'
 
+
 const Layout:React.FC = ({children}) => {
   return (
     <div>
         <Navbar/>
         {children}
-        <footer>this is the footer</footer>
+        <footer className='container'>this is the footer</footer>
+        
+        <style jsx>{`
+            .container{
+                background: salmon
+            }
+        `}</style>
     </div>
   )
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import Layout from '@components/Layout/Layout'
 import type { AppProps } from 'next/app'
 
+
 export default function MyApp({ Component, pageProps }: AppProps) {
 
     //Providers - Context/Providers, Theme, data
